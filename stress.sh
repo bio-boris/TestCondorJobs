44c9facd87ce18946692d2065596d81be3b4775a
bio-boris <bio-boris@users.noreply.github.com>
2018-10-09 12:30:54 -0500

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# file name: sleep.sh
+# file name: stress.sh
 
 cd /mnt/awe/condor/bsadkhin/stress
+
 filename=`date +%s%N`
 filename="$filename.stress.out"
 filename_error="$filename.stress.error"
-echo $filename
-./stress --vm 1 --cpu 1 --io 1 --vm-bytes 60GB --timeout 90s --vm-hang 30 > $filename 2> $filename_error
 
-echo "Test Complete" >> $filename
+echo "Test Begin" > $filename
+./consume17gb  >> $filename
+test_exit_code=$?
+echo "Test Complete with exit code $test_exit_code" >> $filename
